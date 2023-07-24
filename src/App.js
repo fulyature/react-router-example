fcab8954afaf8a22b0475d92e07c70c832484f1c
fulya_ture <fulyature@gmail.com>
2023-07-24 17:24:27 +0300

    initialized

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import People from "./pages/People";
+import NotFound from "./pages/NotFound";
+import { Route, Routes } from "react-router-dom";
+import Path from "./pages/Path";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/paths" element={<Path />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
